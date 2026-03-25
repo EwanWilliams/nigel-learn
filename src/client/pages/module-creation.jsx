5b39eb8e7a0a91c3260c65c8d81b7804bfd5cdea
2boylc17 <2boylc17@solent.ac.uk>
2026-03-25 14:27:51 +0000

    Page created

---
+++ src/client/pages/module-creation.jsx
@@ -0,0 +1,11 @@
+
+
+export default function ModuleCreation() {
+  return (
+    <div>
+        <h1>Module Creation</h1>
+        <p>This is where you can create new modules for your application.</p>
+        
+    </div>
+  );
+}
