788fb1547bd7b975fb507a8928e5c2e073870c27
Ewan Williams <ewan@ewan.name>
2026-03-27 16:10:16 +0000

    Updated Module schema to match formattting

--- src/server/models/Module.mjs
+++ src/server/models/Module.mjs
@@ -1,37 +1,95 @@
 import mongoose from 'mongoose';
 
 const mailSchema = new mongoose.Schema({
-    label: {type: String, required: true, maxLength: 20},
-    type: {type: String, required: true, enum: ['expense', 'income', 'info']},
-    sender: {type: String, required: true, maxLength: 30},
-    date: {type: Date, required: true},
-    subject: {type: String, required: true, maxLength: 50},
-    body: {type: String, required: true, maxLength: 300},
-    amount: {type: Number}
+    label: {
+        type: String,
+        required: true,
+        maxLength: 20
+    },
+    type: {
+        type: String,
+        required: true,
+        enum: ['expense', 'income', 'info']
+    },
+    sender: {
+        type: String,
+        required: true,
+        maxLength: 30
+    },
+    date: {
+        type: Date,
+        required: true
+    },
+    subject: {
+        type: String,
+        required: true,
+        maxLength: 50
+    },
+    body: {
+        type: String,
+        required: true,
+        maxLength: 300
+    },
+    amount: {
+        type: Number
+    }
 });
 
 const incomeSchema = new mongoose.Schema({
-    label: {type: String, required: true, maxLength: 20},
-    category: {type: String, required: true, enum: ['paye', 'invoice', 'casual']},
-    amount: {type: Number, required: true}
+    label: {
+        type: String,
+        required: true,
+        maxLength: 20
+    },
+    category: {
+        type: String,
+        required: true,
+        enum: ['paye', 'invoice', 'casual']
+    },
+    amount: {
+        type: Number,
+        required: true
+    }
 });
 
 const expenseSchema = new mongoose.Schema({
-    label: {type: String, required: true, maxLength: 20},
-    category: {type: String, required: true, enum: ['rent', 'travel', 'food', 'phone', 'subscriptions', 'savings', 'fun', 'other']},
-    amount: {type: Number, required: true} 
+    label: {
+        type: String,
+        required: true,
+        maxLength: 20
+    },
+    category: {
+        type: String,
+        required: true,
+        enum: ['rent', 'travel', 'food', 'phone', 'subscriptions', 'savings', 'fun', 'other']
+    },
+    amount: {
+        type: Number,
+        required: true
+    } 
 });
 
 const weekSchema = new mongoose.Schema({
-    dateStarting: {type: Date, required: true},
+    dateStarting: {
+        type: Date,
+        required: true
+    },
     mailPool: [mailSchema],
     incomePool: [incomeSchema],
     expensePool: [expenseSchema]
 });
 
 const moduleSchema = new mongoose.Schema({
-    title: {type: String, required: true, maxLength: 30},
-    brief: {type: String, required: true, maxLength: 1000},
+    title: {
+        type: String,
+        required: true,
+        maxLength: 30
+    },
+    brief: {
+        type: String,
+        required: true,
+        maxLength: 1000
+    },
     weekPool: [weekSchema]
 });
 
