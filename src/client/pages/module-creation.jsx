655569eb9a4dc08d3a77f6dde113c627016f4ee7
2boylc17 <2boylc17@solent.ac.uk>
2026-03-28 18:08:54 +0000

    Creation page started

--- src/client/pages/module-creation.jsx
+++ src/client/pages/module-creation.jsx
@@ -5,7 +5,16 @@ export default function ModuleCreation() {
     <div>
         <h1>Module Creation</h1>
         <p>This is where you can create new modules for your application.</p>
-        
+
+        <form onSubmit={null}>
+            <label>
+                Module Name:
+                <input type="text" name="moduleName" />
+            </label>
+            <br></br>
+            <br></br>
+            <button type="submit">Create Module</button>
+        </form>
     </div>
   );
 }
