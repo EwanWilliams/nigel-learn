8614f1bc4114337e9e70602d7fa0ebbc20c9b046
Tim-reno88 <6reynt80@solent.ac.uk>
2026-04-08 14:10:03 +0100

    Add teacher routes for direct page links

--- src/client/main.jsx
+++ src/client/main.jsx
@@ -1,9 +1,34 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route, useSearchParams } from "react-router-dom";
 import App from "./App.jsx";
+import TeacherDashboard from "./teacher/pages/TeacherDashboard.jsx";
+import CreateClassroomPage from "./teacher/pages/CreateClassroomPage.jsx";
+import TeacherClassesPage from "./teacher/pages/TeacherClassesPage.jsx";
+import ModuleSelectionPage from "./teacher/pages/ModuleSelectionPage.jsx";
+import ClassroomDetailsPage from "./teacher/pages/ClassroomDetailsPage.jsx";
+
+function TeacherClassesRoute() {
+    const [params] = useSearchParams();
+    return <TeacherClassesPage initialUsername={params.get("username") ?? ""} />;
+}
+
+function ClassroomDetailsRoute() {
+    const [params] = useSearchParams();
+    return <ClassroomDetailsPage initialClassId={params.get("id") ?? ""} />;
+}
 
 ReactDOM.createRoot(document.getElementById("root")).render(
     <React.StrictMode>
-        <App />
+        <BrowserRouter>
+            <Routes>
+                <Route path="/teacher" element={<TeacherDashboard />} />
+                <Route path="/teacher/create" element={<CreateClassroomPage />} />
+                <Route path="/teacher/classes" element={<TeacherClassesRoute />} />
+                <Route path="/teacher/modules" element={<ModuleSelectionPage />} />
+                <Route path="/teacher/classroom" element={<ClassroomDetailsRoute />} />
+                <Route path="/*" element={<App />} />
+            </Routes>
+        </BrowserRouter>
     </React.StrictMode>
 )
