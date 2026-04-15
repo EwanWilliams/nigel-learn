3abc19feaf9ab178221dc8a62c33f8fc846e44d9
Tim-reno88 <6reynt80@solent.ac.uk>
2026-04-15 16:04:53 +0100

    chore: align vite config with main

--- src/vite.config.mjs
+++ src/vite.config.mjs
@@ -10,7 +10,7 @@ export default defineConfig({
         strictPort: true,
         proxy: {
             '/api': {
-                target: `http://localhost:${process.env._PORT}`,
+                taget: `http://localhost:${process.env._PORT}`,
                 changeOrigin: true,
             },
         },
