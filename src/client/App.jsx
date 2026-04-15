6f45fea8d633742134363f2b0bd9df148dcf9d4f
kaceylaird <6lairk77@solent.ac.uk>
2026-04-15 11:05:23 +0100

    fixed app calling and moved browser router into app.jsx

--- src/client/App.jsx
+++ src/client/App.jsx
@@ -1,18 +1,21 @@
-import { Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./app.css";
 
-import LandingPage from "./screens/landing";
-import StudyPage from "./screens/study";
-import TeachPage from "./screens/teach";
-import BuildPage from "./screens/build";
+import LandingPage from "./pages/landing_page";
+import StudyPage from "./pages/study_page";
+import TeachPage from "./pages/teach_page";
+import BuildPage from "./pages/build_page";
 
 export default function App() {
   return (
-    <Routes>
-      <Route path="/" element={<LandingPage />} />
-      <Route path="/study" element={<StudyPage />} />
-      <Route path="/teach" element={<TeachPage />} />
-      <Route path="/build" element={<BuildPage />} />
-    </Routes>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+        <Route path="/study" element={<StudyPage />} />
+        <Route path="/teach" element={<TeachPage />} />
+        <Route path="/build" element={<BuildPage />} />
+        <Route path="*" element={<LandingPage />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
