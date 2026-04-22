a4308248d84f88757e2e1d3d2c223798f4909e70
kaceylaird <6lairk77@solent.ac.uk>
2026-04-22 18:33:34 +0100

    merged main into branch

--- src/client/App.jsx
+++ src/client/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./app.css";
 
 import LandingPage from "./screens/landing";
+import JoinPage from "./screens/join";
 import StudyPage from "./screens/study";
 import TeachPage from "./screens/teach";
 import BuildPage from "./screens/build";
